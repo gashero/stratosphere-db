90d25f46588c607e4adece55ecab83ec18af2151
harry <harry.python@gmail.com>
2011-11-08 19:56:00 +0800

    Try to python implementation.

---
+++ python_mmap/mc_protocol.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+# File: mc_protocol.py
+# Date: 2011-10-24
+# Author: gashero
+
+"""
+Memcached protocol implemented
+"""
+
+import os
+import sys
+import socket
+import select
+
---
+++ python_mmap/sdb_server.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+# File: sdb_server.py
+# Date: 2011-10-24
+# Author: gashero
+
+"""
+Stratosphere Database Server
+Using memcached protocol
+"""
+
+import os
+import sys
+import re
+import mmap
