b1647c51a3d2affbf5ed16de157ca9c875d12996
harry <harry.python@gmail.com>
2011-12-22 15:38:20 +0800

    storage engine is finished, and unittest is passwd.

--- python_mmap/sdb.py
+++ python_mmap/sdb.py
@@ -71,6 +71,7 @@ class SDBWriter(object):
         return rec
 
     def _set_rec(self,pos,rec):
+        assert len(rec)==8
         self.indexfile[pos*64:(pos+1)*64]=INDEX_RECORD.pack(*rec)
         return
 
@@ -93,18 +94,49 @@ class SDBWriter(object):
         #write index
         vertex_record=[chunk_id,0,0,0,0,0,0,0]
         self._set_rec(index_id,vertex_record)
-        self.indexfile.flush()
-        self.chunkfile.flush()
+        #self.indexfile.flush()
+        #self.chunkfile.flush()
         return index_id
+
+    def append_pointer(self,rid_from,rid_to):
+        objpos=rid_from
+        #get last record
+        while True:
+            rec=self._get_rec(objpos)
+            if rec[-1]!=0:
+                objpos=rec[-1]
+            else:
+                break
+        rec=filter(lambda x:x!=0,rec)
+        if len(rec)<7:
+            rec.append(rid_to)
+            rec+=[0]*(8-len(rec))
+            self._set_rec(objpos,rec)
+        else:           #record is full
+            #inc index id
+            meta_record=self._get_rec(0)
+            index_id=meta_record[0]
+            meta_record[0]=index_id+1
+            self._set_rec(0,meta_record)
+            rec[-1]=index_id
+            self._set_rec(objpos,rec)
+            new_ext_rec=[-1,rid_to,0,0,0,0,0]
+            self._set_rec(index_id,new_ext_rec)
+        return
 
     def update_pointer(self,rid_from,rid_to1,rid_to2):
         objpos=rid_from
         while True:
             rec=self._get_rec(objpos)
-            for (pos,iid) in zip(range(1,6),rec[1:-1]):
-                if iid==rid_to1:
-                    rec[pos]==rid_to2
-                    break
+            dataset=[]
+            if rid_to1 in rec[1:-1]:
+                for iid in rec[1:-1]:
+                    if iid==rid_to1:
+                        dataset.append(rid_to2)
+                    else:
+                        dataset.append(iid)
+                rec=rec[:1]+dataset+rec[-1:]
+                break
             else:
                 if rec[-1]!=0:
                     objpos=rec[-1]
@@ -113,40 +145,13 @@ class SDBWriter(object):
         self._set_rec(objpos,rec)
         return
 
-    def append_pointer(self,rid_from,rid_to):
-        objpos=rid_from
-        while True:
-            rec=self._get_rec(objpos)
-            if rec[-1]!=0:
-                objpos=rec[-1]
-            else:
-                break
-        for pos in range(6,1):
-            if rec[pos]!=0:
-                break
-        if pos!=6:  #record is not full
-            rec[pos]=rid_to
-            self._set_rec(objpos,rec)
-        else:
-            #inc index id
-            meta_record=self._get_rec(0)
-            index_id=meta_record[0]
-            meta_record[0]=index_id+1
-            self._set_rec(0,meta_record)
-            rec[-1]=index_id
-            self._set_rec(objpos,rec)
-            new_ext_rec=[0,rid_to,0,0,0,0,0]
-            self._set_rec(index_id,new_ext_rec)
-        return
-
     def delete_pointer(self,rid_from,rid_to):
         objpos=rid_from
         while True:
             rec=self._get_rec(objpos)
-            for (pos,iid) in zip(range(1,6),rec[1:6]):
-                if iid==rid_to:
-                    rec[pos]=0
-                    break
+            if rid_to in rec[1:-1]:
+                rec=rec[:1]+filter(lambda x:x!=rid_to,rec[1:-1])+[0]+rec[-1:]
+                break
             else:
                 if rec[-1]!=0:
                     objpos=rec[-1]
@@ -181,6 +186,25 @@ class SDBReader(object):
         chunk=self.chunkfile[pos+8:pos+chunklen+8]
         return chunk
 
+    def _filter_pointer(self,rid,func):
+        objpos=rid
+        retlist=[]
+        while True:
+            rec=self._get_rec(objpos)
+            for iid in rec[1:-1]:
+                if iid==0:
+                    continue
+                prec=self._get_rec(iid)
+                s=self._get_chunk(prec[0])
+                #print prec,repr(s)
+                if func(s):
+                    retlist.append(s)
+            if rec[-1]!=0:
+                objpos=rec[-1]
+            else:
+                break
+        return retlist
+
     def locate_record(self,key,value):
         pos=1
         s=key+'='+value
@@ -196,13 +220,17 @@ class SDBReader(object):
             pos+=1
         raise NotFoundError('key&value not exists')
 
-    def prefix_pointer(self,key,value,prefix=None):
-        #TODO:
-        return
+    def prefix_pointer(self,rid,prefix=None):
+        if prefix:
+            retlist=self._filter_pointer(rid,lambda x:x.startswith(prefix))
+        else:
+            retlist=self._filter_pointer(rid,lambda x:True)
+        return retlist
 
-    def regex_pointer(self,key,value,regex=None):
-        #TODO:
-        return
+    def regex_pointer(self,rid,regex=None):
+        pattern=re.compile(regex)
+        retlist=self._filter_pointer(rid,lambda x:pattern.match(x))
+        return retlist
 
 ## Unittest ####################################################################
 
@@ -228,11 +256,11 @@ class TestOthers(unittest.TestCase):
         #print time_finish-time_start
         return
 
-class TestSDBWriter(unittest.TestCase):
+class TestSDBWriterReader(unittest.TestCase):
 
     def setUp(self):
-        self.sdbw=SDBWriter('/tmp/redo.log',1024**2,1024**2)
-        self.sdbr=SDBReader(1024**2,1024**2)
+        self.sdbw=SDBWriter('/tmp/redo.log',1024*2,1024*2)
+        self.sdbr=SDBReader(1024*2,1024*2)
         return
 
     def tearDown(self):
@@ -262,18 +290,31 @@ class TestSDBWriter(unittest.TestCase):
         #    print '%d\t%s\t%s'%(i,repr(self.sdbw._get_rec(i)),
         #        self.sdbr._get_rec(i))
         self.assertEqual(self.sdbr.locate_record('name','hliu'),2)
+        self.assertEqual(self.sdbr.locate_record('name','h4'),5)
         return
 
-    def test_update_pointer(self):
-        #TODO:
-        return
-
-    def test_append_pointer(self):
-        #TODO:
-        return
-
-    def test_delete_pointer(self):
-        #TODO:
+    def test_append_update_delete_pointer(self):
+        rid_harry=self.sdbw.new_record('name','harry')
+        rid_hliu=self.sdbw.new_record('name','hliu')
+        rid_age29=self.sdbw.new_record('age','29')
+        rid_age30=self.sdbw.new_record('age','30')
+        rid_male=self.sdbw.new_record('sex','male')
+        rid_age31=self.sdbw.new_record('age','31')
+        self.sdbw.append_pointer(rid_harry,rid_age29)
+        self.sdbw.append_pointer(rid_harry,rid_age30)
+        self.sdbw.append_pointer(rid_harry,rid_male)
+        self.sdbw.append_pointer(rid_hliu,rid_age29)
+        self.assertEqual(self.sdbw._get_rec(rid_harry)[1:],
+                [rid_age29,rid_age30,rid_male,0,0,0,0])
+        self.assertEqual(self.sdbw._get_rec(rid_hliu)[1:],
+                [rid_age29,0,0,0,0,0,0])
+        self.assertEqual(self.sdbr.locate_record('name','harry'),1)
+        self.assertEqual(self.sdbr.prefix_pointer(rid_harry,'age='),['age=29','age=30'])
+        self.assertEqual(self.sdbr.prefix_pointer(rid_harry),['age=29','age=30','sex=male'])
+        self.sdbw.update_pointer(rid_harry,rid_age29,rid_age31)
+        self.assertEqual(self.sdbr.prefix_pointer(rid_harry,'age='),['age=31','age=30'])
+        self.sdbw.delete_pointer(rid_harry,rid_age31)
+        self.assertEqual(self.sdbr.prefix_pointer(rid_harry,'age='),['age=30'])
         return
 
 if __name__=='__main__':
