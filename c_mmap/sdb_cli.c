5cfdc7c7d37cc3073e598019fa05d3822776c67f
gashero <harry.python@gmail.com>
2012-03-09 17:09:11 +0800

    Start writing c version of sdb. Building Makefile and basic files.

---
+++ c_mmap/sdb_cli.c
@@ -0,0 +1,13 @@
+/*
+ * File: sdb_cli.c
+ * Date: 2012-03-09
+ * Author: gashero <harry.python@gmail.com>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    printf("Hello, world!\n");
+    return 0;
+}
---
+++ c_mmap/sdb_core.c
@@ -0,0 +1,5 @@
+/*
+ * File: sdb_core.c
+ * Date: 2012-03-09
+ * Author: gashero <harry.python@gmail.com>
+ */
